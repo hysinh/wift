8a805a493003d8a1dc96dc57f34112265c8a4298
Ngocdzung Smith <hysinh1@gmail.com>
2025-01-22 10:16:49 +0000

    Fix: Correct syntax error to connect static files

--- wift/urls.py
+++ wift/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # django admin panel and all-auth authentication pages
@@ -23,4 +25,4 @@ urlpatterns = [
 
     # public pages
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
