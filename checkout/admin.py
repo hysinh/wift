801c80a02964816757827b35d047302e96bebec1
hysinh <hysinh1@gmail.com>
2025-01-30 18:39:42 +0000

    Add checkout Membership Purchase model to admin and add method to display correct purchase price

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
+from .models import MembershipPurchase
+
+
+@admin.register(MembershipPurchase)
+class MembershipPurchaseAdmin(admin.ModelAdmin):
+    readonly_fields = ('purchase_number', 'purchase_date',
+                       'purchase_total', 'stripe_pid')
+    
+    fields = ('purchase_number', 'member', 'purchase_date',
+              'membership_purchased', 'purchase_total',
+              'stripe_pid')
+    
+    order = ('-date',)
+
+
+
 
 
 
--- checkout/models.py
+++ checkout/models.py
@@ -10,7 +10,7 @@ class MembershipPurchase(models.Model):
     member = models.ForeignKey(User, on_delete=models.RESTRICT, null=False, blank=False)
     membership_purchased = models.ForeignKey(MembershipCategory, on_delete=models.RESTRICT, null=False, blank=False)
     purchase_date = models.DateTimeField(auto_now_add=True)
-    purchase_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    purchase_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_purchase_number(self):
@@ -21,11 +21,20 @@ class MembershipPurchase(models.Model):
     
     def save(self, *args, **kwargs):
         """
-        Overrises the original save method to set the purchase number if it hasn't already been set
+        Overrides the original save method to set the purchase number if it hasn't 
+        already been set and sets the purchase price
         """
         if not self.purchase_number:
             self.purchase_number = self._generate_purchase_number()
+
+        self.purchase_total = self.membership_purchased.new_member_price
+        
         super().save(*args, **kwargs)
+
+    def update_purchase_total(self):
+        """ Updates the purchase total dependent on the membership level selected """
+        self.purchase_total = self.membership_purchased.new_member_price
+        self.save()
     
     def __str__(self):
         return self.purchase_number
