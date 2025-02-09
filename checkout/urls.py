ee52263ad4371e8ae884ad4f9ef6bc00dd328d07
hysinh <hysinh1@gmail.com>
2025-02-09 11:27:44 +0000

    Add checkout_test template, view and urls for testing

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/', views.checkout, name='checkout'),
+    # path('checkout/', views.checkout, name='checkout'),
+    path('checkout/test/', views.checkout_test, name='checkout_test'),
     path('checkout_success/<purchase_number>', views.checkout_success, name='checkout_success'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -10,114 +10,156 @@ import stripe
 import json
 
 
-def checkout(request):
+# def checkout(request):
+#     stripe_public_key = settings.STRIPE_PUBLIC_KEY
+#     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+#     # delete - printing to check values
+#     # basket = request.session.get('basket', {})
+#     # print(basket)
+#     # for category_id, quantity in basket.items():
+#     #     category_id = category_id
+#     #     quantity = quantity
+#     # print(category_id)
+#     # print(quantity)
+#     # selected_membership = get_object_or_404(MembershipCategory, pk=category_id)
+#     # print(selected_membership.name)
+#     # purchase_total = selected_membership.new_member_price * quantity
+#     # print(purchase_total)
+#     # enddelete
+#     if request.method == "POST":
+#         basket = request.session.get("basket", {})
+#         print(basket)
+#         for category_id, quantity in basket.items():
+#             category_id = category_id
+#             quantity = quantity
+#         print(category_id)
+#         print(quantity)
+#         selected_membership = get_object_or_404(MembershipCategory, pk=category_id)
+#         print(selected_membership.name)
+#         purchase_total = selected_membership.new_member_price * quantity
+#         print(purchase_total)
+#         purchase_form = MembershipPurchaseForm()
+#         # the form data for the Membership Private Data form
+#         form_data = {
+#             "default_firstname": request.POST["default_firstname"],
+#             "default_lastname": request.POST["default_lastname"],
+#             "default_street_address1": request.POST["default_street_address1"],
+#             "default_street_address2": request.POST["default_street_address2"],
+#             "default_town_or_city": request.POST["default_town_or_city"],
+#             "default_county": request.POST["default_county"],
+#             "default_postcode": request.POST["default_postcode"],
+#             "default_country": request.POST["default_country"],
+#         }
+
+#         member_info_form = MembershipPrivateDataForm(form_data)
+#         if purchase_form.is_valid() and member_info_form.is_valid():
+#             purchase = purchase_form.save(commit=False)
+#             member_info = member_info_form.save(commit=False)
+#             # sets the request.user as the member
+#             purchase.member = request.user
+#             member_info.member = request.user
+#             print(request.user)
+#             # sets the membership level purchased
+#             purchase.membership_purchased = selected_membership
+#             member_info.membership_level = selected_membership
+#             print(member_info.membership_level)
+#             purchase.purchase_total = selected_membership.new_member_price * quantity
+#             pid = request.POST.get("client_secret").split("_secret")[0]
+#             purchase.stripe_pid = pid
+#             print(pid)
+#             purchase.save()
+#             member_info.save()
+#             return redirect(
+#                 reverse('checkout_success', args=[purchase.purchase_number])
+#             )
+#         else:
+#             messages.error(
+#                 request,
+#                 "There was an error with your form.\
+#                            Please double check your information.",
+#             )
+#     else:
+#         basket = request.session.get("basket", {})
+#         current_basket = basket_contents(request)
+
+#         if not basket:
+#             messages.error(request, "There's nothing in your basket at the moment")
+#             return redirect(reverse("join"))
+
+#         current_basket = basket_contents(request)
+#         total = current_basket["total"]
+#         print(total)
+#         stripe_total = round(total * 100)
+#         print(stripe_total)
+#         stripe.api_key = stripe_secret_key
+#         intent = stripe.PaymentIntent.create(
+#             amount=stripe_total,
+#             currency=settings.STRIPE_CURRENCY,
+#         )
+#         print(intent)
+#         member_data_form = MembershipPrivateDataForm()
+#         purchase_form = MembershipPurchaseForm()
+
+#         if not stripe_public_key:
+#             messages.warning(
+#                 request,
+#                 "Stripe public key is missing. \
+#                 Did you forget to set it in your environment?",
+#             )
+
+#         template = "checkout/checkout.html"
+#         context = {
+#             "member_data_form": member_data_form,
+#             "purchase_form": purchase_form,
+#             "stripe_public_key": stripe_public_key,
+#             "client_secret": intent.client_secret,
+#         }
+
+#         return render(request, template, context)
+
+
+def checkout_test(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    # delete - printing to check values
-    # basket = request.session.get('basket', {})
-    # print(basket)
-    # for category_id, quantity in basket.items():
-    #     category_id = category_id
-    #     quantity = quantity
-    # print(category_id)
-    # print(quantity)
-    # selected_membership = get_object_or_404(MembershipCategory, pk=category_id)
-    # print(selected_membership.name)
-    # purchase_total = selected_membership.new_member_price * quantity
-    # print(purchase_total)
-    # enddelete
-    if request.method == "POST":
-        basket = request.session.get("basket", {})
-        print(basket)
-        for category_id, quantity in basket.items():
-            category_id = category_id
-            quantity = quantity
-        print(category_id)
-        print(quantity)
-        selected_membership = get_object_or_404(MembershipCategory, pk=category_id)
-        print(selected_membership.name)
-        purchase_total = selected_membership.new_member_price * quantity
-        print(purchase_total)
-        purchase_form = MembershipPurchaseForm()
-        # the form data for the Membership Private Data form
-        form_data = {
-            "default_firstname": request.POST["default_firstname"],
-            "default_lastname": request.POST["default_lastname"],
-            "default_street_address1": request.POST["default_street_address1"],
-            "default_street_address2": request.POST["default_street_address2"],
-            "default_town_or_city": request.POST["default_town_or_city"],
-            "default_county": request.POST["default_county"],
-            "default_postcode": request.POST["default_postcode"],
-            "default_country": request.POST["default_country"],
-        }
+    basket = request.session.get("basket", {})
 
-        member_info_form = MembershipPrivateDataForm(form_data)
-        if purchase_form.is_valid() and member_info_form.is_valid():
-            purchase = purchase_form.save(commit=False)
-            member_info = member_info_form.save(commit=False)
-            # sets the request.user as the member
-            purchase.member = request.user
-            member_info.member = request.user
-            print(request.user)
-            # sets the membership level purchased
-            purchase.membership_purchased = selected_membership
-            member_info.membership_level = selected_membership
-            print(member_info.membership_level)
-            purchase.purchase_total = selected_membership.new_member_price * quantity
-            pid = request.POST.get("client_secret").split("_secret")[0]
-            purchase.stripe_pid = pid
-            print(pid)
-            purchase.save()
-            member_info.save()
-            return redirect(
-                reverse('checkout_success', args=[purchase.purchase_number])
-            )
-        else:
-            messages.error(
-                request,
-                "There was an error with your form.\
-                           Please double check your information.",
-            )
-    else:
-        basket = request.session.get("basket", {})
-        current_basket = basket_contents(request)
+    if not basket:
+        messages.error(request, "There's nothing in your basket at the moment")
+        return redirect(reverse("join"))
+    
+    current_basket = basket_contents(request)
+    total = current_basket['total']
+    print(total)
+    stripe_total = round(total * 100)
+    print(stripe_total)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+    member_data_form = MembershipPrivateDataForm()
+    purchase_form = MembershipPurchaseForm()
 
-        if not basket:
-            messages.error(request, "There's nothing in your basket at the moment")
-            return redirect(reverse("join"))
-
-        current_basket = basket_contents(request)
-        total = current_basket["total"]
-        print(total)
-        stripe_total = round(total * 100)
-        print(stripe_total)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            "Stripe public key is missing. \
+            Did you forget to set it in your environment?",
         )
-        print(intent)
-        member_data_form = MembershipPrivateDataForm()
-        purchase_form = MembershipPurchaseForm()
 
-        if not stripe_public_key:
-            messages.warning(
-                request,
-                "Stripe public key is missing. \
-                Did you forget to set it in your environment?",
-            )
+    template = "checkout/checkout_test.html"
+    context = {
+        "member_data_form": member_data_form,
+        "purchase_form": purchase_form,
+        "stripe_public_key": stripe_public_key,
+        "client_secret": intent.client_secret,
+    }
 
-        template = "checkout/checkout.html"
-        context = {
-            "member_data_form": member_data_form,
-            "purchase_form": purchase_form,
-            "stripe_public_key": stripe_public_key,
-            "client_secret": intent.client_secret,
-        }
-
-        return render(request, template, context)
-
+    return render(request, template, context)
+    
 
 def checkout_success(request, purchase_number):
     """ Handle successful checkouts """
