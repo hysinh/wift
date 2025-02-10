14a86a2dc16f47c6b8fe5d04d5dea1603c694ee1
hysinh <hysinh1@gmail.com>
2025-02-10 19:17:33 +0000

    Update contexts to include renewal price for existing members. Update checkout success

--- basket/contexts.py
+++ basket/contexts.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from membership.models import MembershipCategory
+from profiles.models import Member_Data_Private
 
 
 def basket_contents(request):
@@ -11,23 +12,32 @@ def basket_contents(request):
 
     basket = request.session.get('basket', {})
 
-    for category_id, quantity in basket.items():
-        selected_membership_level = get_object_or_404(MembershipCategory, pk=category_id)
-        total += quantity * selected_membership_level.new_member_price
-        product_count += quantity
-        basket_items.append({
-            'category_id': category_id,
-            'quantity': quantity,
-            'selected_membership_level': selected_membership_level,
-            'price': selected_membership_level.new_member_price
-        })
+    user = request.user
 
-    # if total < settings.FREE_DELIVERY_THRESHOLD:
-    #     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    #     free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    # else:
-    #     delivery = 0
-    #     free_delivery_delta = 0
+    existing_member = get_object_or_404(Member_Data_Private, member=request.user)
+
+    if existing_member:
+        for category_id, quantity in basket.items():
+            selected_membership_level = get_object_or_404(MembershipCategory, pk=category_id)
+            total += quantity * selected_membership_level.renewal_price
+            product_count += quantity
+            basket_items.append({
+                'category_id': category_id,
+                'quantity': quantity,
+                'selected_membership_level': selected_membership_level,
+                'price': selected_membership_level.renewal_price
+            })
+    else:
+        for category_id, quantity in basket.items():
+            selected_membership_level = get_object_or_404(MembershipCategory, pk=category_id)
+            total += quantity * selected_membership_level.new_member_price
+            product_count += quantity
+            basket_items.append({
+                'category_id': category_id,
+                'quantity': quantity,
+                'selected_membership_level': selected_membership_level,
+                'price': selected_membership_level.new_member_price
+            })
 
     context = {
         'basket_items': basket_items,
--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
+    path('checkout/existing/', views.checkout_existing_member, name='checkout_existing_member'),
     path('checkout_success/<purchase_number>', views.checkout_success, name='checkout_success'),
 ]
