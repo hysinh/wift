24aaa6ba25a5ede588d1d71acd0cbfb7bb5ee311
hysinh <hysinh1@gmail.com>
2025-02-13 21:31:32 +0000

    Update environment variables for heroku deployment

--- wift/settings.py
+++ wift/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7d#w4p6kr#yueat@@l!%qwg*xbr%qzf)*talt0f&5b-x#*@w#7'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
