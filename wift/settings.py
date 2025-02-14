2c46a085a1e66f25f75059441f1e602768c37980
hysinh <hysinh1@gmail.com>
2025-02-14 11:17:50 +0000

    Adjust debug value in settings to check for development

--- wift/settings.py
+++ wift/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',  # local preview
