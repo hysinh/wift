4f4165ae5e1f13cce79a049e1867f16dde7dd646
hysinh <hysinh1@gmail.com>
2025-02-14 11:34:38 +0000

    Install whitemoise and collect static for heroku deployment

---
+++ staticfiles/checkout/js/stripe_elements.js
@@ -0,0 +1,97 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>`;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+            // purchase_details: {
+            //     purchase_number: $.trim(purchase_form.purchase_number.value),
+            //     member: $.trim(purchase_form.member.value),
+            //     membership_purchased: $.trim(purchase_form.membership_purchased.value),
+            //     purchase_date: $.trim(purchase_form.purchase_date.value),
+            //     purchase_total: $.trim(purchase_form.purchase_total.value),
+            // }
+        }
+        // },
+        // member_details: {
+        //     member: $.trim(member_data_form.member.value),
+        //     membership_level: $.trim(member_data_form.membership_level.value),
+        //     firstname: $.trim(member_data_form.default_firstname.value),
+        //     lastname: $.trim(member_data_form.default_lastname.value),
+        //     address: {
+        //         line1: $.trim(member_data_form.default_street_address1.value),
+        //         line2: $.trim(member_data_form.default_street_address2.value),
+        //         city: $.trim(member_data_form.default_town_or_city.value),
+        //         country: $.trim(member_data_form.default_country.value),
+        //         postal_code: $.trim(member_data_form.default_postcode.value),
+        //         state: $.trim(member_data_form.default_county.value),
+        //     }
+        // },
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }  
+        }
+    });
+});
