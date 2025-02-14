b0c7f893ef35dcba4bc609cdd385c9a5994cca8a
hysinh <hysinh1@gmail.com>
2025-02-14 14:11:28 +0000

    Add purchase details and member details values from checkout form to stripe elements js file

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -54,29 +54,26 @@ form.addEventListener('submit', function(ev) {
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
-            // purchase_details: {
-            //     purchase_number: $.trim(purchase_form.purchase_number.value),
-            //     member: $.trim(purchase_form.member.value),
-            //     membership_purchased: $.trim(purchase_form.membership_purchased.value),
-            //     purchase_date: $.trim(purchase_form.purchase_date.value),
-            //     purchase_total: $.trim(purchase_form.purchase_total.value),
-            // }
-        }
-        // },
-        // member_details: {
-        //     member: $.trim(member_data_form.member.value),
-        //     membership_level: $.trim(member_data_form.membership_level.value),
-        //     firstname: $.trim(member_data_form.default_firstname.value),
-        //     lastname: $.trim(member_data_form.default_lastname.value),
-        //     address: {
-        //         line1: $.trim(member_data_form.default_street_address1.value),
-        //         line2: $.trim(member_data_form.default_street_address2.value),
-        //         city: $.trim(member_data_form.default_town_or_city.value),
-        //         country: $.trim(member_data_form.default_country.value),
-        //         postal_code: $.trim(member_data_form.default_postcode.value),
-        //         state: $.trim(member_data_form.default_county.value),
-        //     }
-        // },
+            purchase_details: {
+                purchase_number: $.trim(purchase_form.purchase_number.value),
+                member: $.trim(purchase_form.member.value),
+                membership_purchased: $.trim(purchase_form.membership_purchased.value),
+                purchase_date: $.trim(purchase_form.purchase_date.value),
+                purchase_total: $.trim(purchase_form.purchase_total.value),
+            }
+        },
+        member_details: {
+            member: $.trim(member_data_form.member.value),
+            membership_level: $.trim(member_data_form.membership_level.value),
+            default_firstname: $.trim(member_data_form.default_firstname.value),
+            default_lastname: $.trim(member_data_form.default_lastname.value),
+            default_street_address1: $.trim(member_data_form.default_street_address1.value),
+            default_street_address2: $.trim(member_data_form.default_street_address2.value),
+            default_town_or_city: $.trim(member_data_form.default_town_or_city.value),
+            default_county: $.trim(member_data_form.default_county.value),
+            default_postcode: $.trim(member_data_form.default_postcode.value),
+            default_country: $.trim(member_data_form.default_country.value),
+        },
     }).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
