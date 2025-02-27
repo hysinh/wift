c8c95ecbeae3a2ea36a34fd054636bd92ffc1cf8
hysinh <hysinh1@gmail.com>
2025-02-27 11:19:03 +0000

    Apply prettier formatter to basket app files

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from membership.models import MembershipCategory
@@ -10,39 +9,47 @@ def basket_contents(request):
     total = 0
     product_count = 0
 
-    basket = request.session.get('basket', {})
+    basket = request.session.get("basket", {})
 
     existing_member = Member_Data_Private.objects.filter(member=request.user.id)
 
     if existing_member.exists():
         for category_id, quantity in basket.items():
-            selected_membership_level = get_object_or_404(MembershipCategory, pk=category_id)
+            selected_membership_level = get_object_or_404(
+                MembershipCategory, pk=category_id
+            )
             total += quantity * selected_membership_level.renewal_price
             product_count += quantity
-            basket_items.append({
-                'category_id': category_id,
-                'quantity': quantity,
-                'selected_membership_level': selected_membership_level,
-                'price': selected_membership_level.renewal_price
-            })
-    
+            basket_items.append(
+                {
+                    "category_id": category_id,
+                    "quantity": quantity,
+                    "selected_membership_level": selected_membership_level,
+                    "price": selected_membership_level.renewal_price,
+                }
+            )
+
     else:
         for category_id, quantity in basket.items():
-            selected_membership_level = get_object_or_404(MembershipCategory, pk=category_id)
+            selected_membership_level = get_object_or_404(
+                MembershipCategory, pk=category_id
+            )
             total += quantity * selected_membership_level.new_member_price
             product_count += quantity
-            basket_items.append({
-                'category_id': category_id,
-                'quantity': quantity,
-                'selected_membership_level': selected_membership_level,
-                'price': selected_membership_level.new_member_price
-            })
+            basket_items.append(
+                {
+                    "category_id": category_id,
+                    "quantity": quantity,
+                    "selected_membership_level": selected_membership_level,
+                    "price": selected_membership_level.new_member_price,
+                }
+            )
 
     context = {
-        'basket_items': basket_items,
-        'total': total,
-        'product_count': product_count,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        "basket_items": basket_items,
+        "total": total,
+        "product_count": product_count,
+        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
     }
 
     return context
--- basket/urls.py
+++ basket/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('basket/', views.view_basket, name='view_basket'),
-    path('add/<category_id>', views.add_to_basket, name='add_to_basket'),
-    path('remove/<category_id>/', views.remove_from_basket, name='remove_from_basket'),
+    path("basket/", views.view_basket, name="view_basket"),
+    path("add/<category_id>", views.add_to_basket, name="add_to_basket"),
+    path("remove/<category_id>/", views.remove_from_basket, name="remove_from_basket"),
 ]
