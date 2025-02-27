8df568de4913d532be34015b3ef839af492aace9
hysinh <hysinh1@gmail.com>
2025-02-27 14:21:12 +0000

    Remove print statement from webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -120,7 +120,6 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
-        print(f"event: {event}")
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
