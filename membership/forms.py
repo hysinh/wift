1ed55efb827665f3f13b3e7a7acefdd8c956c28a
hysinh <hysinh1@gmail.com>
2025-03-01 11:16:06 +0000

    Remove unused Registration form in membership app

--- membership/forms.py
+++ membership/forms.py
@@ -1,9 +1,4 @@
 from django import forms
-from .models import MembershipCategory
 
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = MembershipCategory
-        fields = ["name"]
 
-    name = forms.ModelChoiceField(queryset=MembershipCategory.objects.all(), label='Membership Level')
+# Your form goes here
