7348dde4eb83db294bde73a66f9853a355a3252d
hysinh <hysinh1@gmail.com>
2025-03-01 11:07:00 +0000

    Edit spacing and line lengths in home app python files

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Contact
 
+
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(SummernoteModelAdmin):
